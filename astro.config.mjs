d985c9cb91ac6eb2e6f9d9bd333d1cbd9716a7cf
Sopronyi Zoltán <kissopa02@gmail.com>
2026-05-11 20:17:26 +0200

    configured starlight, added c4 views, created sidebars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,60 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import react from "@astrojs/react";
+import { LikeC4VitePlugin } from "likec4/vite-plugin";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  site: "https://unideb-advanced-software-engineering.github.io",
+  base: "/26-tavasz-01-mundamail",
+  vite: {
+    plugins: [LikeC4VitePlugin({})],
+  },
+  integrations: [
+    starlight({
+      title: "MundaMail",
+      defaultLocale: "hu",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/unideb-advanced-software-engineering/26-tavasz-01-mundamail",
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+      sidebar: [
+        {
+          label: "Esettanulmány",
+          slug: "esettanulmany",
+        },
+        {
+          label: "SRS",
+          link: "/srs-mundamail.docx",
+        },
+        {
+          label: "Architekturális karakterisztikák",
+          slug: "ac",
+        },
+        {
+          label: "Architekturálisan szignifikáns követelmények",
+          slug: "asr",
+        },
+        {
+          label: "Architekturális stílus",
+          slug: "as",
+        },
+        {
+          label: "C4 diagramok",
+          slug: "c4",
+        },
+        {
+          label: "Döntési jegyzőkönyvek",
+          items: [{ autogenerate: { directory: "adr" } }],
+        },
+      ],
+    }),
+    react(),
+  ],
 });
